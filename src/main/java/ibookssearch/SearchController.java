aedbe79878f27d2ccc8a78dc01aecc209ab056dc
marclafratta <marclafratta@gmail.com>
2018-05-02 22:36:39 -0400

    moved to jsp on /

--- src/main/java/ibookssearch/SearchController.java
+++ src/main/java/ibookssearch/SearchController.java
@@ -22,27 +22,9 @@ public class SearchController {
     @Value("${application.message:Hello World}")
     private String message = "Hello World";
 
-    @GetMapping("/searchpage")
+    @GetMapping("/")
     public String welcome(Map<String, Object> model) {
-
-        Quote quote = restTemplate.getForObject(
-        "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-
-        ITunesSearchResponse searchResponse = restTemplate.getForObject("http://itunes.apple.com/search?term=Aubrey+Marcus&country=ca&media=ebook",
-                ITunesSearchResponse.class);
-
-        System.out.println(quote);
-
-        System.out.println(searchResponse);
-
-        model.put("quote", quote.getValue().getQuote());
-        //model.put("quote", "testquote");
-        model.put("time", new Date());
-        model.put("message", this.message);
-
-        model.put("searchResponse", searchResponse);
-
-        return "hello";
+        return "search";
     }
 
 }
--- src/main/webapp/WEB-INF/views/search.jsp
+++ src/main/webapp/WEB-INF/views/search.jsp
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="en">
 
 <head>
     <meta charset="utf-8" />
@@ -9,6 +13,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
         crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="book-icon.png">
 </head>
 
 <body>
